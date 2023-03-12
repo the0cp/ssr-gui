4c2d6343c1054b14286c99cfbbd23015bfa6b533
Theodore Cooper <63190431+the0cp@users.noreply.github.com>
2023-03-12 14:26:01 +0000

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+  chainWebpack: config => {
+    config
+      .entry('app')
+      .clear()
+      .add('./src/render/main.js')
+      .end()
+    config.resolve.alias.set('@', path.join(__dirname, './src/render'))
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        files: [
+          {
+            filter: ['**/*'],
+          },
+        ],
+        asar: true,
+      },
+      mainProcessFile: 'src/main/index.js',
+      mainProcessWatch: ['src/main'],
+    },
+  },
+  productionSourceMap: false,
+}
